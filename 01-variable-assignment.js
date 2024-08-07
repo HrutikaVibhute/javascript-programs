4d3112b04657755a502a9e0d789c83478487b332
HrutikaVibhute <hrutikavibhute23@gmail.com>
2024-08-07 14:51:12 +0530

    javascript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("Hrutika Vibhute");
+console.log("my city name is hingoli");
+console.log("my bestfriend name is sakshi");
+console.log("my school name is mjp hingoli");
+console.log("my college name is ASM IBMR pune");
